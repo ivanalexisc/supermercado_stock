683df5313c5cbb070d4d6ed3e916a371bcd1dba8
ALEo14 <aleedin14@gmail.com>
2025-04-10 12:31:06 -0300

    Routes

---
+++ frontend/frontend_tienda/src/Routes/AppRoutes.tsx
@@ -0,0 +1,17 @@
+
+
+import { Routes, Route } from 'react-router-dom';
+import Products from '../pages/Product';
+import Home from "../pages/home"
+const AppRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/products" element={<Products />} />
+        <Route path="/Home" element={<Home/>} />
+    </Routes>
+    </div>
+  )
+}
+
+export default AppRoutes
